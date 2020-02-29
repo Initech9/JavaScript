f88414c14d80a56168e6c4d67ee42ced575a4079
Initech9 <ag9876@gmail.com>
2020-02-29 10:11:19 -0800

    Sort names in descending order in JavaScript

---
+++ js/rectangles.js
@@ -0,0 +1,12 @@
+
+// Arrays start at 0,1,2,3
+var names = ["James", "George", "Steve", "Austin"];
+document.getElementById("app").innerHTML = names;
+
+function myFunction() {
+  // Sort the names in the array
+  names.sort();
+  // Reverse the order of the names:
+  names.reverse();
+  document.getElementById("app").innerHTML = names;
+}
